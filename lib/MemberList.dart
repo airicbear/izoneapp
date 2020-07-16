fbfd5f96347841e0db9534d9460033ec468ee3ac
Eric Nguyen <tuk94307@temple.edu>
2020-07-15 22:25:46 -0400

    Make MemberList use SliverAppBar

--- lib/MemberList.dart
+++ lib/MemberList.dart
@@ -16,45 +16,55 @@ class MemberList extends StatelessWidget {
         child: MediaButtons(),
         color: Theme.of(context).primaryColor,
       ),
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Expanded(
-              child: Text(title),
-            ),
-            Text(
-              'Debut Oct. 29, 2018',
-              style: TextStyle(color: Theme.of(context).disabledColor),
-            ),
-          ],
-        ),
-      ),
       body: Container(
         child: FutureBuilder(
           future:
               DefaultAssetBundle.of(context).loadString('assets/izone.json'),
           builder: (context, snapshot) {
             var members = json.decode(snapshot.data.toString());
-            return GridView.builder(
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                childAspectRatio: 1.15,
-                maxCrossAxisExtent: 256,
-              ),
-              itemCount: members == null ? 0 : members.length,
-              itemBuilder: (context, index) {
-                return MemberTile(
-                  member: Member(
-                    id: members[index]['ID'],
-                    birthplace: members[index]['BIRTHPLACE'],
-                    dateOfBirth: members[index]['DATEOFBIRTH'],
-                    fullName: members[index]['FULLNAME'],
-                    fullNameKorean: members[index]['FULLNAME_KR'],
-                    nationality: members[index]['NATIONALITY'],
-                    stageName: members[index]['STAGENAME'],
-                    stageNameKorean: members[index]['STAGENAME_KR'],
+            return CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  title: Row(
+                    children: [
+                      Expanded(
+                        child: Text(title),
+                      ),
+                      Text(
+                        'Debut Oct. 29, 2018',
+                        style:
+                            TextStyle(color: Theme.of(context).disabledColor),
+                      ),
+                    ],
                   ),
-                );
-              },
+                ),
+                SliverGrid(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    childAspectRatio: 1.0,
+                    maxCrossAxisExtent: 256.0,
+                  ),
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: MemberTile(
+                          member: Member(
+                            id: members[index]['ID'],
+                            birthplace: members[index]['BIRTHPLACE'],
+                            dateOfBirth: members[index]['DATEOFBIRTH'],
+                            fullName: members[index]['FULLNAME'],
+                            fullNameKorean: members[index]['FULLNAME_KR'],
+                            nationality: members[index]['NATIONALITY'],
+                            stageName: members[index]['STAGENAME'],
+                            stageNameKorean: members[index]['STAGENAME_KR'],
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: members.length,
+                  ),
+                ),
+              ],
             );
           },
         ),
