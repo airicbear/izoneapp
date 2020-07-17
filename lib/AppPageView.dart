2d711f53fafa733389f2a125b11de63232898163
Eric Nguyen <tuk94307@temple.edu>
2020-07-17 15:30:56 -0400

    Add about dialog

--- lib/AppPageView.dart
+++ lib/AppPageView.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:izoneapp/pages/AboutPage.dart';
 import 'package:izoneapp/MediaButtons.dart';
 import 'package:izoneapp/pages/MembersPage.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 enum Page { ABOUT, MEMBERS }
+enum More { ABOUT }
 
 class AppPageView extends StatefulWidget {
   AppPageView({Key key}) : super(key: key);
@@ -57,6 +60,56 @@ class _AppPageViewState extends State<AppPageView> {
           _pageTitle(context, 'Members', Page.MEMBERS),
         ],
       ),
+      actions: [
+        PopupMenuButton(
+          onSelected: (More result) {
+            switch (result) {
+              case More.ABOUT:
+                showAboutDialog(
+                  context: context,
+                  applicationIcon: Image.asset(
+                    'assets/launcher_round.png',
+                    width: 64,
+                  ),
+                  children: [
+                    Text(
+                        '''This is an unofficial fan-made app dedicated to IZ*ONE and is free and open-source for everyone.'''),
+                    Divider(height: 24),
+                    Text(
+                        '''If you want to contribute to this app, please visit the GitHub page at github.com/airicbear/izone-app using the button below.'''),
+                    FlatButton.icon(
+                      label: Text('airicbear/izone-app'),
+                      icon: FaIcon(FontAwesomeIcons.github),
+                      onPressed: () =>
+                          launch('https://github.com/airicbear/izone-app'),
+                    ),
+                    Divider(height: 24),
+                    Text(
+                        '''If you want to support the development of this app, you can pay through PayPal using the button below.'''),
+                    FlatButton.icon(
+                      label: Text('airicbear'),
+                      icon: FaIcon(FontAwesomeIcons.paypal),
+                      onPressed: () =>
+                          launch('https://www.paypal.me/airicbear'),
+                    ),
+                  ],
+                  applicationLegalese: '''
+MIT License
+
+Copyright (c) 2020 Eric Nguyen
+                  ''',
+                );
+                break;
+            }
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuEntry<More>>[
+            const PopupMenuItem<More>(
+              value: More.ABOUT,
+              child: Text('About this app'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
