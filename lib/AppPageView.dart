797fe5ada0538be31eecb5a9a9928a50f3a7834f
Eric Nguyen <tuk94307@temple.edu>
2020-07-17 16:25:34 -0400

    Add disclaimer page

--- lib/AppPageView.dart
+++ lib/AppPageView.dart
@@ -6,7 +6,7 @@ import 'package:izoneapp/pages/MembersPage.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 enum Page { ABOUT, MEMBERS }
-enum More { ABOUT }
+enum More { DISCLAIMER, ABOUT }
 
 class AppPageView extends StatefulWidget {
   AppPageView({Key key}) : super(key: key);
@@ -64,6 +64,89 @@ class _AppPageViewState extends State<AppPageView> {
         PopupMenuButton(
           onSelected: (More result) {
             switch (result) {
+              case More.DISCLAIMER:
+                showDialog(
+                  context: context,
+                  child: Scaffold(
+                    appBar: AppBar(
+                      title: Text('Disclaimer'),
+                    ),
+                    body: Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            'Content rights',
+                            textScaleFactor: 2.0,
+                          ),
+                          subtitle: Text(
+                              'This is an unofficial fan-made app. This fan app and its content are not officially endorsed or produced by, nor associated with or affiliated with the music artist(s) or any associated entities of the artist(s), such as management or record label. All trademarks and copyrights are property of their respective owners.'),
+                        ),
+                        Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                'Content accuracy',
+                                textScaleFactor: 2.0,
+                              ),
+                              subtitle: Text(
+                                  'The content in this app references multiple different sources. I strive to get the most accurate and up-to-date information as possible however it is not always possible. If you find that there is inaccurate information in this app please submit an issue on the GitHub page providing your sources and I will try to fix it as soon as possible.'),
+                            ),
+                            ButtonBar(
+                              children: [
+                                _githubButton('Submit an issue'),
+                              ],
+                            )
+                          ],
+                        ),
+                        ListTile(
+                          title: Text(
+                            'References',
+                            textScaleFactor: 2.0,
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  'There are several different sources referenced in this app including'),
+                              FlatButton.icon(
+                                onPressed: () => launch('http://iz-one.co.kr/'),
+                                icon: FaIcon(FontAwesomeIcons.globeAmericas),
+                                label: Text('The official IZ*ONE website'),
+                              ),
+                              FlatButton.icon(
+                                onPressed: () =>
+                                    launch('https://www.izone-official.com/'),
+                                icon: FaIcon(FontAwesomeIcons.globeAmericas),
+                                label:
+                                    Text('The official IZ*ONE Japan website'),
+                              ),
+                              FlatButton.icon(
+                                onPressed: () => launch(
+                                    'https://kprofiles.com/izone-members-profile/'),
+                                icon: FaIcon(FontAwesomeIcons.globeAmericas),
+                                label: Text(
+                                    'IZ*ONE Members Profile at KProfiles.com'),
+                              ),
+                              FlatButton.icon(
+                                onPressed: () => launch(
+                                    'https://kpop.fandom.com/wiki/IZ*ONE'),
+                                icon: FaIcon(FontAwesomeIcons.globeAmericas),
+                                label: Text('IZ*ONE FANDOM KPOP Wiki Page'),
+                              ),
+                              FlatButton.icon(
+                                onPressed: () =>
+                                    launch('https://dbkpop.com/group/izone'),
+                                icon: FaIcon(FontAwesomeIcons.globeAmericas),
+                                label: Text('dbkpop.com'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+                break;
               case More.ABOUT:
                 showAboutDialog(
                   context: context,
@@ -73,19 +156,14 @@ class _AppPageViewState extends State<AppPageView> {
                   ),
                   children: [
                     Text(
-                        '''This is an unofficial fan-made app dedicated to IZ*ONE and is free and open-source for everyone.'''),
+                        'This is an unofficial fan-made app dedicated to IZ*ONE and is free and open-source for everyone.'),
                     Divider(height: 24),
                     Text(
-                        '''If you want to contribute to this app, please visit the GitHub page at github.com/airicbear/izone-app using the button below.'''),
-                    FlatButton.icon(
-                      label: Text('airicbear/izone-app'),
-                      icon: FaIcon(FontAwesomeIcons.github),
-                      onPressed: () =>
-                          launch('https://github.com/airicbear/izone-app'),
-                    ),
+                        'If you want to contribute to this app, please visit the GitHub page at github.com/airicbear/izone-app using the button below.'),
+                    _githubButton('airicbear/izone-app'),
                     Divider(height: 24),
                     Text(
-                        '''If you want to support the development of this app, you can pay through PayPal using the button below.'''),
+                        'If you want to support the development of this app, you can pay through PayPal using the button below.'),
                     FlatButton.icon(
                       label: Text('airicbear'),
                       icon: FaIcon(FontAwesomeIcons.paypal),
@@ -103,6 +181,10 @@ Copyright (c) 2020 Eric Nguyen
             }
           },
           itemBuilder: (BuildContext context) => <PopupMenuEntry<More>>[
+            const PopupMenuItem<More>(
+              value: More.DISCLAIMER,
+              child: Text('Disclaimer'),
+            ),
             const PopupMenuItem<More>(
               value: More.ABOUT,
               child: Text('About this app'),
@@ -138,6 +220,14 @@ Copyright (c) 2020 Eric Nguyen
         child: MediaButtons(),
         color: Theme.of(context).primaryColor,
       ),
+    );
+  }
+
+  Widget _githubButton(String label) {
+    return FlatButton.icon(
+      label: Text(label),
+      icon: FaIcon(FontAwesomeIcons.github),
+      onPressed: () => launch('https://github.com/airicbear/izone-app'),
     );
   }
 }
