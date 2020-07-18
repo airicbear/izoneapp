a99901efe7880d35a67a7b186ed0fc025a2f767a
Eric Nguyen <tuk94307@temple.edu>
2020-07-18 17:46:43 -0400

    Add more dance videos
    The embedded V Live video is quite buggy:
    - it opens the video in an external browser if a V Live video has
    already been played
    - it literally borks my phone
    (i.e. i cant do anything except use the power button)
    the only way to fix it is to rerun the app from my laptop
    
    Also I'm not really sure how to get autoplay working,
    tried appending "?autoplay=1" to the url but that didn't work

--- lib/pages/DancePage.dart
+++ lib/pages/DancePage.dart
@@ -5,10 +5,10 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class VideoCard {
   final String title;
   final String subtitle;
+  final String date;
   final String youtubeUrl;
-  final String vliveUrl;
 
-  VideoCard({this.title, this.subtitle, this.youtubeUrl, this.vliveUrl});
+  VideoCard({this.title, this.subtitle, this.date, this.youtubeUrl});
 }
 
 class DancePage extends StatefulWidget {
@@ -29,14 +29,92 @@ class DancePageState extends State<DancePage> {
       VideoCard(
         title: 'Secret Story of the Swan',
         subtitle: 'Dance practice',
+        date: '2020-06-23',
         youtubeUrl: 'https://www.youtube.com/embed/1yQjsWhOuJI',
-        vliveUrl: 'https://www.vlive.tv/embed/199011',
+      ),
+      VideoCard(
+        title: 'Pretty',
+        subtitle: 'Dance Performance',
+        date: '2020-06-19',
+        youtubeUrl: 'https://www.youtube.com/embed/o3KQAKchUT8',
+      ),
+      VideoCard(
+        title: 'Secret Story of the Swan',
+        subtitle: 'STUDIO CHOOM performance',
+        date: '2020-06-16',
+        youtubeUrl: 'https://www.youtube.com/embed/fXSXUKXuqu8',
+      ),
+      VideoCard(
+        title: 'SPACESHIP',
+        subtitle: 'Choreography (To WIZ*ONE ver.)',
+        date: '2020-03-06',
+        youtubeUrl: 'https://www.youtube.com/embed/xaWafVLlIZs',
+      ),
+      VideoCard(
+        title: 'SPACESHIP',
+        date: '2020-03-06',
+        subtitle: 'Choreography (Close up ver.)',
+        youtubeUrl: 'https://www.youtube.com/embed/GsEo4_9mIMg',
       ),
       VideoCard(
         title: 'FIESTA',
         subtitle: 'Dance practice',
+        date: '2020-02-28',
         youtubeUrl: 'https://www.youtube.com/embed/kCk8x__pXvE',
-        vliveUrl: 'https://www.vlive.tv/embed/177616',
+      ),
+      VideoCard(
+        title: 'Vampire',
+        subtitle: 'Dance practice',
+        date: '2019-09-25',
+        youtubeUrl: 'https://www.youtube.com/embed/vr9h-epSUZQ',
+      ),
+      VideoCard(
+        title: 'Up',
+        subtitle: 'Dance practice',
+        date: '2019-05-17',
+        youtubeUrl: 'https://www.youtube.com/embed/IsUVaQOj0fs',
+      ),
+      VideoCard(
+        title: 'Violeta',
+        subtitle: 'Dance practice (Close up ver.)',
+        date: '2019-04-25',
+        youtubeUrl: 'https://www.youtube.com/embed/wnTWDHiBRpQ',
+      ),
+      VideoCard(
+        title: 'Violeta',
+        subtitle: 'Dance practice',
+        date: '2019-04-07',
+        youtubeUrl: 'https://www.youtube.com/embed/Z8_aiEDLoY4',
+      ),
+      VideoCard(
+        title: 'La Vie en Rose',
+        subtitle: '2018 MAMA ver.',
+        date: '2019-01-04',
+        youtubeUrl: 'https://www.youtube.com/embed/xhdW-GceM6g',
+      ),
+      VideoCard(
+        title: 'O\' My!',
+        subtitle: 'Dance practice',
+        date: '2018-11-07',
+        youtubeUrl: 'https://www.youtube.com/embed/56XuG4JrByk',
+      ),
+      VideoCard(
+        title: 'La Vie en Rose',
+        subtitle: 'Dance practice',
+        date: '2018-11-03',
+        youtubeUrl: 'https://www.youtube.com/embed/hsMaygLZ3G4',
+      ),
+      VideoCard(
+        title: 'Rumor',
+        subtitle: 'SHOW-CON spoiler',
+        date: '2018-10-19',
+        youtubeUrl: 'https://www.youtube.com/embed/h49TeJo4mK0',
+      ),
+      VideoCard(
+        title: 'PICK ME',
+        subtitle: 'Dance practice (12 ver.)',
+        date: '2018-10-04',
+        youtubeUrl: 'https://www.youtube.com/embed/YGiLQUc8x5E',
       ),
     ];
 
@@ -44,18 +122,20 @@ class DancePageState extends State<DancePage> {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            expandedHeight: 250,
-            collapsedHeight: 200,
+            expandedHeight: 240,
+            collapsedHeight: 240,
+            floating: true,
+            pinned: true,
+            elevation: 0,
             flexibleSpace: _currentVideo.isEmpty
                 ? Padding(
                     padding: const EdgeInsets.all(64.0),
                     child: Center(
-                      child: Text(
-                          'Select a video to watch by clicking on its V Live or YouTube button.'),
+                      child: Text('Select a video to watch.'),
                     ),
                   )
                 : HtmlWidget(
-                    "<iframe src='$_currentVideo' frameborder='no' scrolling='no' marginwidth='0' marginheight='0' WIDTH='544' HEIGHT='306' allowfullscreen></iframe>",
+                    '<iframe width="560" height="315" src="$_currentVideo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
                     webView: true,
                   ),
           ),
@@ -63,32 +143,25 @@ class DancePageState extends State<DancePage> {
             delegate: SliverChildBuilderDelegate(
               (context, index) {
                 return Card(
-                  child: Stack(
-                    children: [
-                      ListTile(
-                        title: Text('${videos[index].title}'),
-                        subtitle: videos[index].subtitle == null
-                            ? null
-                            : Text(videos[index].subtitle),
-                      ),
-                      ButtonBar(
-                        children: [
-                          IconButton(
-                            icon:
-                                ImageIcon(AssetImage('assets/icons/vlive.png')),
-                            onPressed: () => setState(() {
-                              _currentVideo = videos[index].vliveUrl;
-                            }),
-                          ),
-                          IconButton(
-                            icon: FaIcon(FontAwesomeIcons.youtube),
-                            onPressed: () => setState(() {
-                              _currentVideo = videos[index].youtubeUrl;
-                            }),
-                          ),
-                        ],
-                      )
-                    ],
+                  color: _currentVideo == videos[index].youtubeUrl
+                      ? Theme.of(context).backgroundColor
+                      : Theme.of(context).cardColor,
+                  child: InkWell(
+                    onTap: () => setState(() {
+                      _currentVideo = videos[index].youtubeUrl;
+                    }),
+                    splashFactory: InkRipple.splashFactory,
+                    child: ListTile(
+                      leading: FaIcon(FontAwesomeIcons.youtube),
+                      title: Text('${videos[index].title}'),
+                      subtitle: videos[index].subtitle == null
+                          ? null
+                          : Text(videos[index].subtitle),
+                      trailing: Text(videos[index].date != null
+                          ? MaterialLocalizations.of(context).formatCompactDate(
+                              DateTime.parse(videos[index].date))
+                          : 'Date'),
+                    ),
                   ),
                 );
               },
