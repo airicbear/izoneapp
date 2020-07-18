f33911211c5008483c157a341a876cad9f1f6011
Eric Nguyen <tuk94307@temple.edu>
2020-07-17 23:59:12 -0400

    Add Korean Localization for AboutPage

--- lib/generated/intl/messages_ko.dart
+++ lib/generated/intl/messages_ko.dart
@@ -22,6 +22,7 @@ class MessageLookup extends MessageLookupByLibrary {
   final messages = _notInlinedMessages(_notInlinedMessages);
   static _notInlinedMessages(_) => <String, Function> {
     "about" : MessageLookupByLibrary.simpleMessage("대해"),
+    "aboutIzone" : MessageLookupByLibrary.simpleMessage("IZ*ONE(아이즈원)은 대한민국 음악전문채널 엠넷의 오디션 프로그램《PRODUCE101》과 일본의 국민적 아이돌《AKB48그룹》이 합동으로 기획한　오디션 프로그램《PRODUCE48》에서 팬들의 투표로 선발된 일본인 멤버3명, 한국인 멤버9명 총12명으로 구성된 글로벌 그룹이다.\n\n2018년 10월 19일 한국에서 발매된 데뷔 앨범 \"COLOR*IZ\"는 대한민국 역대 걸즈그룹 데뷔앨범 판매량 최고기록을 갱신하였으며 음원차트에서는 세계10개국에서 1위를 기록.일본에서도 2019년 주목 아티스트로서 여러 미디어에서 특집으로 다루어지며 2019년 2월 6일, 아키모토 야스시가 프로듀스한 \"Suki to Iwasetai\"로 데뷔."),
     "members" : MessageLookupByLibrary.simpleMessage("회원")
   };
 }
