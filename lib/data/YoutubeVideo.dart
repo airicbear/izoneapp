3c6fb043457e2cd5e89312282a706c3d9216eadd
Eric Nguyen <tuk94307@temple.edu>
2020-07-19 00:24:31 -0400

    Refactor Dance videos

---
+++ lib/data/YoutubeVideo.dart
@@ -0,0 +1,8 @@
+class YoutubeVideo {
+  final String title;
+  final String subtitle;
+  final String date;
+  final String youtubeUrl;
+
+  YoutubeVideo({this.title, this.subtitle, this.date, this.youtubeUrl});
+}
--- lib/pages/DancePage.dart
+++ lib/pages/DancePage.dart
@@ -4,17 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:izoneapp/data/DanceVideos.dart';
+import 'package:izoneapp/data/YoutubeVideo.dart';
 import 'package:izoneapp/pages/ViewYoutubeVideoPage.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-class VideoCard {
-  final String title;
-  final String subtitle;
-  final String date;
-  final String youtubeUrl;
-
-  VideoCard({this.title, this.subtitle, this.date, this.youtubeUrl});
-}
 
 class DancePage extends StatefulWidget {
   const DancePage({Key key, this.pageController}) : super(key: key);
@@ -27,102 +20,10 @@ class DancePage extends StatefulWidget {
 
 class DancePageState extends State<DancePage> {
   String _currentVideo = '';
+  List<YoutubeVideo> _videos = DanceVideos.videos;
 
   @override
   Widget build(BuildContext context) {
-    List<VideoCard> videos = [
-      VideoCard(
-        title: 'Secret Story of the Swan',
-        subtitle: 'Dance practice',
-        date: '2020-06-23',
-        youtubeUrl: 'https://www.youtube.com/embed/1yQjsWhOuJI',
-      ),
-      VideoCard(
-        title: 'Pretty',
-        subtitle: 'Dance performance',
-        date: '2020-06-19',
-        youtubeUrl: 'https://www.youtube.com/embed/o3KQAKchUT8',
-      ),
-      VideoCard(
-        title: 'Secret Story of the Swan',
-        subtitle: 'STUDIO CHOOM performance',
-        date: '2020-06-16',
-        youtubeUrl: 'https://www.youtube.com/embed/fXSXUKXuqu8',
-      ),
-      VideoCard(
-        title: 'SPACESHIP',
-        subtitle: 'Choreography (To WIZ*ONE ver.)',
-        date: '2020-03-06',
-        youtubeUrl: 'https://www.youtube.com/embed/xaWafVLlIZs',
-      ),
-      VideoCard(
-        title: 'SPACESHIP',
-        date: '2020-03-06',
-        subtitle: 'Choreography (Close up ver.)',
-        youtubeUrl: 'https://www.youtube.com/embed/GsEo4_9mIMg',
-      ),
-      VideoCard(
-        title: 'FIESTA',
-        subtitle: 'Dance practice',
-        date: '2020-02-28',
-        youtubeUrl: 'https://www.youtube.com/embed/kCk8x__pXvE',
-      ),
-      VideoCard(
-        title: 'Vampire',
-        subtitle: 'Dance practice',
-        date: '2019-09-25',
-        youtubeUrl: 'https://www.youtube.com/embed/vr9h-epSUZQ',
-      ),
-      VideoCard(
-        title: 'Up',
-        subtitle: 'Dance practice',
-        date: '2019-05-17',
-        youtubeUrl: 'https://www.youtube.com/embed/IsUVaQOj0fs',
-      ),
-      VideoCard(
-        title: 'Violeta',
-        subtitle: 'Dance practice (Close up ver.)',
-        date: '2019-04-25',
-        youtubeUrl: 'https://www.youtube.com/embed/wnTWDHiBRpQ',
-      ),
-      VideoCard(
-        title: 'Violeta',
-        subtitle: 'Dance practice',
-        date: '2019-04-07',
-        youtubeUrl: 'https://www.youtube.com/embed/Z8_aiEDLoY4',
-      ),
-      VideoCard(
-        title: 'La Vie en Rose',
-        subtitle: '2018 MAMA ver.',
-        date: '2019-01-04',
-        youtubeUrl: 'https://www.youtube.com/embed/xhdW-GceM6g',
-      ),
-      VideoCard(
-        title: 'O\' My!',
-        subtitle: 'Dance practice',
-        date: '2018-11-07',
-        youtubeUrl: 'https://www.youtube.com/embed/56XuG4JrByk',
-      ),
-      VideoCard(
-        title: 'La Vie en Rose',
-        subtitle: 'Dance practice',
-        date: '2018-11-03',
-        youtubeUrl: 'https://www.youtube.com/embed/hsMaygLZ3G4',
-      ),
-      VideoCard(
-        title: 'Rumor',
-        subtitle: 'SHOW-CON spoiler',
-        date: '2018-10-19',
-        youtubeUrl: 'https://www.youtube.com/embed/h49TeJo4mK0',
-      ),
-      VideoCard(
-        title: 'PICK ME',
-        subtitle: 'Dance practice (12 ver.)',
-        date: '2018-10-04',
-        youtubeUrl: 'https://www.youtube.com/embed/YGiLQUc8x5E',
-      ),
-    ];
-
     Widget _videoPlaceholder(bool isSliver) {
       try {
         if ((Platform.isAndroid || Platform.isIOS) &&
@@ -147,7 +48,7 @@ class DancePageState extends State<DancePage> {
         delegate: SliverChildBuilderDelegate(
           (context, index) {
             return Card(
-              color: _currentVideo == videos[index].youtubeUrl
+              color: _currentVideo == _videos[index].youtubeUrl
                   ? Theme.of(context).backgroundColor
                   : Theme.of(context).cardColor,
               child: InkWell(
@@ -155,20 +56,20 @@ class DancePageState extends State<DancePage> {
                   try {
                     if (Platform.isAndroid || Platform.isIOS) {
                       setState(() {
-                        _currentVideo = videos[index].youtubeUrl;
+                        _currentVideo = _videos[index].youtubeUrl;
                       });
                     }
                   } catch (e) {
-                    if (await canLaunch(videos[index].youtubeUrl)) {
-                      launch(videos[index].youtubeUrl);
+                    if (await canLaunch(_videos[index].youtubeUrl)) {
+                      launch(_videos[index].youtubeUrl);
                     } else {
-                      throw 'Could not launch ${videos[index].youtubeUrl}.';
+                      throw 'Could not launch ${_videos[index].youtubeUrl}.';
                     }
                   }
                 },
                 splashFactory: InkRipple.splashFactory,
                 child: ListTile(
-                  leading: _currentVideo == videos[index].youtubeUrl
+                  leading: _currentVideo == _videos[index].youtubeUrl
                       ? IconButton(
                           icon: Icon(Icons.fullscreen),
                           onPressed: () {
@@ -177,7 +78,7 @@ class DancePageState extends State<DancePage> {
                                 context,
                                 MaterialPageRoute(
                                   builder: (context) => ViewYoutubeVideoPage(
-                                    youtubeUrl: videos[index].youtubeUrl,
+                                    youtubeUrl: _videos[index].youtubeUrl,
                                   ),
                                 ),
                               );
@@ -189,15 +90,15 @@ class DancePageState extends State<DancePage> {
                           },
                         )
                       : FaIcon(FontAwesomeIcons.youtube),
-                  title: Text('${videos[index].title}'),
-                  subtitle: Text(videos[index].subtitle),
+                  title: Text('${_videos[index].title}'),
+                  subtitle: Text(_videos[index].subtitle),
                   trailing: Text(MaterialLocalizations.of(context)
-                      .formatCompactDate(DateTime.parse(videos[index].date))),
+                      .formatCompactDate(DateTime.parse(_videos[index].date))),
                 ),
               ),
             );
           },
-          childCount: videos.length,
+          childCount: _videos.length,
         ),
       );
     }
