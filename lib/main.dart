ac86efff234274d134c3aa12e958b351e6fda6f4
Eric Nguyen <tuk94307@temple.edu>
2020-07-21 18:42:55 -0400

    Refactor AppPageView to new folder "controllers"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:izoneapp/AppPageView.dart';
+import 'package:izoneapp/controllers/AppPageView.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:izoneapp/generated/l10n.dart';
 
