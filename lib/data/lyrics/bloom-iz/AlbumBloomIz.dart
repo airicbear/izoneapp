f63bb2920178acd270b23dcc595e31532c03b456
Eric Nguyen <tuk94307@temple.edu>
2020-07-22 20:11:09 -0400

    Lyrics: FIESTA

--- lib/data/lyrics/bloom-iz/AlbumBloomIz.dart
+++ lib/data/lyrics/bloom-iz/AlbumBloomIz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:izoneapp/data/Album.dart';
+import 'package:izoneapp/data/lyrics/bloom-iz/LyricsFiesta.dart';
 
 class AlbumBloomIz extends Album {
   AlbumBloomIz(BuildContext context)
@@ -8,7 +9,9 @@ class AlbumBloomIz extends Album {
           title: "BLOOM*IZ",
           releaseDate: "2020-02-17",
           color: Color.fromRGBO(175, 125, 101, 1),
-          songs: [],
+          songs: [
+            LyricsFiesta(context),
+          ],
         );
 
   @override
