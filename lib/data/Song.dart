6813c1bff2d91ca47653076c94ba32bb9a8dfc19
Eric Nguyen <tuk94307@temple.edu>
2020-07-24 17:38:50 -0400

    Ensure lyrics line count matches for each song

--- lib/data/Song.dart
+++ lib/data/Song.dart
@@ -6,5 +6,18 @@ abstract class Song {
   final Duration length;
   final Map<String, List<String>> lyrics;
 
-  Song(this.context, {this.title, this.length, this.lyrics});
+  static bool lineCountMatch(Map<String, List<String>> lyrics) {
+    return (lyrics.length > 1
+            ? lyrics.values.toList()[0].length ==
+                lyrics.values.toList()[1].length
+            : true) &&
+        (lyrics.length > 2
+            ? lyrics.values.toList()[1].length ==
+                lyrics.values.toList()[2].length
+            : true);
+  }
+
+  Song(this.context, {this.title, this.length, this.lyrics})
+      : assert(lyrics.length > 0 && lineCountMatch(lyrics),
+            'Song lyrics must have matching number of lines.');
 }
