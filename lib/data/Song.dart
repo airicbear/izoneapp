708f02d2f5f0d5049270cd5627dce4da00a174ad
Eric Nguyen <tuk94307@temple.edu>
2020-07-28 05:46:25 -0400

    Improve error message for mismatching line count

--- lib/data/Song.dart
+++ lib/data/Song.dart
@@ -17,7 +17,28 @@ abstract class Song {
             : true);
   }
 
+  /// Returns the number of lines for each
+  /// section of a given set of lyrics
+  static String sectionIndices(Map<String, List<String>> lyrics) {
+    return lyrics
+        .map((k, v) {
+          List<int> _indices = [];
+          int i = 0;
+          while (i < v.length) {
+            if (v.elementAt(i).isEmpty ||
+                (v.elementAt(i).startsWith('[') &&
+                    v.elementAt(i).endsWith(']'))) {
+              _indices.add(i);
+            }
+            i++;
+          }
+          return MapEntry(k, '[${_indices.join(', ')}]');
+        })
+        .entries
+        .join('\n');
+  }
+
   Song(this.context, {this.title, this.length, this.lyrics})
       : assert(lyrics.length > 0 && lineCountMatch(lyrics),
-            'Song lyrics must have matching number of lines. The song "$title" has line counts ${lyrics.values.map((e) => e.length)}.');
+            'Song lyrics must have matching number of lines. The song lyrics for "$title" has section indices\n\n${sectionIndices(lyrics)}\n');
 }
