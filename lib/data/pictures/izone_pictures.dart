01f15d6dafd58bff28470a12fb8647bcc66cb767
Eric Nguyen <tuk94307@temple.edu>
2020-07-31 04:16:37 -0400

    Use List.generate instead of hardcoding pictures

--- lib/data/pictures/izone_pictures.dart
+++ lib/data/pictures/izone_pictures.dart
@@ -1,5 +1,6 @@
 import 'package:izoneapp/data/izone_picture.dart';
 import 'package:izoneapp/data/media_content.dart';
+import 'package:izoneapp/data/members/members.dart';
 
 class IzonePictures implements MediaContent<IzonePicture> {
   static List<IzonePicture> _pictures;
@@ -18,93 +19,37 @@ class IzonePictures implements MediaContent<IzonePicture> {
     return _taggedPictures;
   }
 
-  IzonePictures() {
-    _pictures = [
-      IzonePicture(
+  static List<IzonePicture> oneiricDiaryPictures(number, ver) {
+    const _dbkpopUrl = 'https://dbkpop.com/wp-content/uploads';
+    List<String> memberNames = List.generate(
+        Members.members.length,
+        (index) => Members.members
+            .elementAt(index)
+            .fullName
+            .replaceAll(' ', '_')
+            .toLowerCase());
+    return List.generate(
+      memberNames.length,
+      (index) => IzonePicture(
         path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_an_yujin.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) An Yujin',
-        tags: ['yujin', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_jo_yuri.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Jo Yuri',
-        tags: ['yuri', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_kim_chaewon.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Kim Chaewon',
-        tags: ['chaewon', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_kwon_eunbi.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Kwon Eunbi',
-        tags: ['eunbi', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_miyawaki_sakura.jpg',
+            '$_dbkpopUrl/2020/06/izone_oneiric_diary_teaser_$ver${number > 1 ? '_' + number.toString() : ""}_${memberNames.elementAt(index)}.jpg',
         date: DateTime(2020, 06, 03),
         description:
-            'Oneiric Diary Teaser Photo 1 (Diary ver.) Miyawaki Sakura',
-        tags: ['sakura', 'oneiric-diary'],
+            'Oneiric Diary Teaser Photo $number (Diary ver.) ${Members.members.elementAt(index).fullName}',
+        tags: [
+          Members.members.elementAt(index).stageName.toLowerCase(),
+          'oneiric-diary',
+        ],
       ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_kang_hyewon.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Kang Hyewon',
-        tags: ['hyewon', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_choi_yena.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Choi Yena',
-        tags: ['yena', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_lee_chaeyeon.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Lee Chaeyeon',
-        tags: ['chaeyeon', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_kim_minju.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Kim Minju',
-        tags: ['minju', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_yabuki_nako.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Yabuki Nako',
-        tags: ['nako', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_honda_hitomi.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Honda Hitomi',
-        tags: ['hitomi', 'oneiric-diary'],
-      ),
-      IzonePicture(
-        path:
-            'https://dbkpop.com/wp-content/uploads/2020/06/izone_oneiric_diary_teaser_diary_jang_wonyoung.jpg',
-        date: DateTime(2020, 06, 03),
-        description: 'Oneiric Diary Teaser Photo 1 (Diary ver.) Jang Wonyoung',
-        tags: ['wonyoung', 'oneiric-diary'],
-      ),
+    );
+  }
+
+  IzonePictures() {
+    _pictures = [
+      ...oneiricDiaryPictures(1, 'diary'),
+      ...oneiricDiaryPictures(2, 'diary'),
+      ...oneiricDiaryPictures(3, 'diary'),
+      ...oneiricDiaryPictures(4, 'oneiric_diary'),
     ];
   }
 }
