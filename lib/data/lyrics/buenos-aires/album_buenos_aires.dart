b744368daaf0076173184bc782003e87ba3fa060
Eric Nguyen <tuk94307@temple.edu>
2020-08-02 13:13:56 -0400

    Placeholder: copy pasted Buenos Aires lyrics

--- lib/data/lyrics/buenos-aires/album_buenos_aires.dart
+++ lib/data/lyrics/buenos-aires/album_buenos_aires.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:izoneapp/data/album.dart';
 import 'package:izoneapp/data/lyrics/buenos-aires/lyrics_buenos_aires.dart';
+import 'package:izoneapp/data/lyrics/buenos-aires/lyrics_human_love.dart';
+import 'package:izoneapp/data/lyrics/buenos-aires/lyrics_target.dart';
+import 'package:izoneapp/data/lyrics/buenos-aires/lyrics_tomorrow.dart';
+import 'package:izoneapp/data/lyrics/buenos-aires/lyrics_toshita_boyfriend.dart';
 
 class AlbumBuenosAires extends Album {
   AlbumBuenosAires(BuildContext context)
@@ -11,6 +15,10 @@ class AlbumBuenosAires extends Album {
           color: Color.fromRGBO(107, 45, 49, 1),
           songs: [
             LyricsBuenosAires(context),
+            LyricsTomorrow(context),
+            LyricsTarget(context),
+            LyricsToshitaBoyfriend(context),
+            LyricsHumanLove(context),
           ],
         );
 
