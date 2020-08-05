4ba2155c11c8b8a71a27753d3aa73e8ccdecd38c
Eric Nguyen <tuk94307@temple.edu>
2020-08-05 11:18:26 -0400

    Lyrics: Hey. Bae. Like it.

--- lib/data/lyrics/heart-iz/album_heart_iz.dart
+++ lib/data/lyrics/heart-iz/album_heart_iz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:izoneapp/data/album.dart';
+import 'package:izoneapp/data/lyrics/heart-iz/lyrics_hey_bae_like_it.dart';
 import 'package:izoneapp/data/lyrics/heart-iz/lyrics_violeta.dart';
 
 class AlbumHeartIz extends Album {
@@ -10,6 +11,7 @@ class AlbumHeartIz extends Album {
           releaseDate: "2019-04-01",
           color: Color.fromRGBO(189, 201, 189, 1),
           songs: [
+            LyricsHeyBaeLikeIt(context),
             LyricsVioleta(context),
           ],
         );
