69fc415022d9668a7f623d1a7b1fcbf6f40ac270
Eric Nguyen <tuk94307@temple.edu>
2020-08-06 17:09:42 -0400

    Add magenta MaterialColor

--- lib/data/app_themes.dart
+++ lib/data/app_themes.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AppThemes {
+  static MaterialColor get magentaColor => MaterialColor(
+        0xFFEF539D,
+        {
+          50: Color(0xFFFACEE3),
+          100: Color(0xFFF8B5D4),
+          200: Color(0xFFF69CC6),
+          300: Color(0xFFF484B8),
+          400: Color(0xFFF16BAA),
+          500: Color(0xFFef539d),
+          600: Color(0xFFEB2883),
+          700: Color(0xFFD2146C),
+          800: Color(0xFFA81057),
+          900: Color(0xFF7E0C41),
+        },
+      );
+
   static ThemeData get dark => ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.black,
+        primarySwatch: magentaColor,
         accentColor: Colors.purpleAccent,
         cardColor: Color.fromRGBO(35, 35, 35, 1.0),
         canvasColor: Colors.black,
@@ -15,18 +32,19 @@ class AppThemes {
           elevation: 0,
         ),
         primaryColor: Colors.white,
-        primarySwatch: Colors.purple,
+        primarySwatch: magentaColor,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       );
 
   static ThemeData get magenta => ThemeData(
-        primaryColor: Color(0xffef539d),
+        primaryColor: Color(0xFFEF539D),
+        primarySwatch: magentaColor,
         brightness: Brightness.dark,
-        scaffoldBackgroundColor: Color(0xffef539d),
-        accentColor: Color(0xfff7a9cd),
-        cardColor: Color(0xfff16baa),
-        canvasColor: Color(0xfff05fa3),
-        backgroundColor: Color(0xfff05fa3),
+        scaffoldBackgroundColor: Color(0xFFEF539D),
+        accentColor: Color(0xFFF7A9CD),
+        cardColor: Color(0xFFF16BAA),
+        canvasColor: Color(0xFFF05FA3),
+        backgroundColor: Color(0xFFF05FA3),
       );
 
   static Map<String, ThemeData> themes(BuildContext context) {
