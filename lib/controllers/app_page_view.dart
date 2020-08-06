7ab2903e34e6a9587d3e61c7dd5c3f9ab184bdc4
Eric Nguyen <tuk94307@temple.edu>
2020-08-06 16:47:48 -0400

    Replace preferences button with theme selection

--- lib/controllers/app_page_view.dart
+++ lib/controllers/app_page_view.dart
@@ -3,7 +3,6 @@ import 'package:izoneapp/controllers/scrollable_app_bar_scroll_behavior.dart';
 import 'package:izoneapp/data/app_pages.dart';
 import 'package:izoneapp/data/app_themes.dart';
 import 'package:izoneapp/pages/page_disclaimer.dart';
-import 'package:izoneapp/pages/page_preferences.dart';
 import 'package:izoneapp/widgets/buttons_media.dart';
 import 'package:izoneapp/pages/page_about.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -42,6 +41,13 @@ class AppPageViewState extends State<AppPageView> {
     super.dispose();
   }
 
+  Future<void> _changeTheme(String _theme) async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      theme = _prefs.setString('theme', _theme).then((bool success) => _theme);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<AppPageInfo> _appPages = AppPages.pages(
@@ -50,131 +56,150 @@ class AppPageViewState extends State<AppPageView> {
       _appBarController,
     );
     return FutureBuilder<String>(
-        future: theme,
-        builder: (context, snapshot) {
-          ThemeData _themeData =
-              AppThemes.themes(context)[snapshot.data ?? 'Auto'];
-          return Theme(
-            data: _themeData,
-            child: Scaffold(
-              appBar: AppBar(
-                title: _PageViewAppBar(
-                  appPages: _appPages,
-                  pageController: _pageController,
-                  appBarController: _appBarController,
-                  nextPage: _page,
-                ),
-              ),
-              drawer: Drawer(
-                child: ListView(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(top: 12.0),
-                      child: ListTile(
-                        title: Text(
-                          'IZ*ONE Lyrics',
-                          textScaleFactor: 2.0,
-                        ),
-                        subtitle: Text('Unoffical lyrics app'),
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(12.0),
-                          child: Image.asset('assets/launcher.png'),
-                        ),
-                      ),
-                    ),
-                    ...List<Widget>.generate(
-                      _appPages.length,
-                      (index) {
-                        AppPageInfo _appPage = _appPages.elementAt(index);
-                        return InkWell(
-                          onTap: () {
-                            AppPages.goToPage(
-                              _appPage,
-                              _pageController,
-                              _appBarController,
-                            );
-                            Navigator.pop(context);
-                          },
-                          child: ListTile(
-                            leading: _appPage.icon,
-                            title: Text(_appPage.title),
-                          ),
-                        );
-                      },
-                    ),
-                    Divider(
-                      color: _themeData.disabledColor,
-                      height: 24.0,
-                      thickness: 1.0,
-                      indent: 12.0,
-                      endIndent: 12.0,
-                    ),
-                    InkWell(
-                      onTap: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => Theme(
-                            data: _themeData,
-                            child: DisclaimerPage(),
-                          ),
-                        ),
-                      ),
-                      child: ListTile(
-                        leading: Icon(Icons.warning_rounded),
-                        title: Text('Disclaimer'),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => Theme(
-                            data: _themeData,
-                            child: AboutAppPage(),
-                          ),
-                        ),
-                      ),
-                      child: ListTile(
-                        leading: Icon(Icons.info),
-                        title: Text('About this app'),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => PreferencesPage(root: this),
-                        ),
-                      ),
-                      child: ListTile(
-                        leading: Icon(Icons.settings),
-                        title: Text('Preferences'),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              body: PageView(
-                onPageChanged: (index) {
-                  setState(() {
-                    _page = AppPage.values.elementAt(index);
-                  });
-                  AppPages.scrollAppBarToPage(
-                    _appPages.elementAt(index),
-                    _appBarController,
-                  );
-                },
-                controller: _pageController,
-                scrollDirection: Axis.horizontal,
-                children: List.generate(
-                  _appPages.length,
-                  (index) => _appPages.elementAt(index).page,
-                ),
-              ),
-              bottomNavigationBar: BottomAppBar(
-                child: MediaButtons(),
-                color: _themeData.primaryColor,
+      future: theme,
+      builder: (context, snapshot) {
+        ThemeData _themeData =
+            AppThemes.themes(context)[snapshot.data ?? 'Auto'];
+        return Theme(
+          data: _themeData,
+          child: Scaffold(
+            appBar: AppBar(
+              title: _PageViewAppBar(
+                appPages: _appPages,
+                pageController: _pageController,
+                appBarController: _appBarController,
+                nextPage: _page,
               ),
             ),
-          );
-        });
+            drawer: Drawer(
+              child: ListView(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 12.0),
+                    child: ListTile(
+                      title: Text(
+                        'IZ*ONE Lyrics',
+                        textScaleFactor: 2.0,
+                      ),
+                      subtitle: Text('Unoffical lyrics app'),
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: Image.asset('assets/launcher.png'),
+                      ),
+                    ),
+                  ),
+                  ...List<Widget>.generate(
+                    _appPages.length,
+                    (index) {
+                      AppPageInfo _appPage = _appPages.elementAt(index);
+                      return InkWell(
+                        onTap: () {
+                          AppPages.goToPage(
+                            _appPage,
+                            _pageController,
+                            _appBarController,
+                          );
+                          Navigator.pop(context);
+                        },
+                        child: ListTile(
+                          leading: _appPage.icon,
+                          title: Text(_appPage.title),
+                        ),
+                      );
+                    },
+                  ),
+                  Divider(
+                    color: _themeData.disabledColor,
+                    height: 24.0,
+                    thickness: 1.0,
+                    indent: 12.0,
+                    endIndent: 12.0,
+                  ),
+                  InkWell(
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => Theme(
+                          data: _themeData,
+                          child: DisclaimerPage(),
+                        ),
+                      ),
+                    ),
+                    child: ListTile(
+                      leading: Icon(Icons.warning_rounded),
+                      title: Text('Disclaimer'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => Theme(
+                          data: _themeData,
+                          child: AboutAppPage(),
+                        ),
+                      ),
+                    ),
+                    child: ListTile(
+                      leading: Icon(Icons.info),
+                      title: Text('About this app'),
+                    ),
+                  ),
+                  InkWell(
+                    child: ExpansionTile(
+                      leading: Icon(Icons.palette),
+                      title: Text('Themes'),
+                      children: List.generate(
+                        AppThemes.themes(context).length,
+                        (index) {
+                          String _themeName = AppThemes.themes(context)
+                              .keys
+                              .toList()
+                              .elementAt(index);
+                          return InkWell(
+                            onTap: () => _changeTheme(_themeName),
+                            child: ListTile(
+                              title: Text(_themeName),
+                              trailing: Icon(
+                                Icons.circle,
+                                color: AppThemes.themes(context)
+                                    .values
+                                    .toList()
+                                    .elementAt(index)
+                                    .primaryColor,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            body: PageView(
+              onPageChanged: (index) {
+                setState(() {
+                  _page = AppPage.values.elementAt(index);
+                });
+                AppPages.scrollAppBarToPage(
+                  _appPages.elementAt(index),
+                  _appBarController,
+                );
+              },
+              controller: _pageController,
+              scrollDirection: Axis.horizontal,
+              children: List.generate(
+                _appPages.length,
+                (index) => _appPages.elementAt(index).page,
+              ),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              child: MediaButtons(),
+              color: _themeData.primaryColor,
+            ),
+          ),
+        );
+      },
+    );
   }
 }
 
