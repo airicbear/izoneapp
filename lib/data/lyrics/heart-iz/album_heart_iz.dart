c92c91a14105c4d475bae5d09203b70587143a42
Eric Nguyen <tuk94307@temple.edu>
2020-08-07 18:00:44 -0400

    Lyrics: Up

--- lib/data/lyrics/heart-iz/album_heart_iz.dart
+++ lib/data/lyrics/heart-iz/album_heart_iz.dart
@@ -4,6 +4,7 @@ import 'package:izoneapp/data/lyrics/heart-iz/lyrics_airplane.dart';
 import 'package:izoneapp/data/lyrics/heart-iz/lyrics_hey_bae_like_it.dart';
 import 'package:izoneapp/data/lyrics/heart-iz/lyrics_highlight.dart';
 import 'package:izoneapp/data/lyrics/heart-iz/lyrics_really_like_you.dart';
+import 'package:izoneapp/data/lyrics/heart-iz/lyrics_up.dart';
 import 'package:izoneapp/data/lyrics/heart-iz/lyrics_violeta.dart';
 
 class AlbumHeartIz extends Album {
@@ -19,6 +20,7 @@ class AlbumHeartIz extends Album {
             LyricsHighlight(context),
             LyricsReallyLikeYou(context),
             LyricsAirplane(context),
+            LyricsUp(context),
           ],
         );
 
