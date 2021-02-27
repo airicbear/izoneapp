33e0e6029dd603a376a77d025af542f7c7eff7b3
Eric Nguyen <tuk94307@temple.edu>
2021-02-27 10:35:15 -0500

    Add Panorama lyrics

--- lib/data/lyrics/one-reeler-act-iv/album_one_reeler_act_iv.dart
+++ lib/data/lyrics/one-reeler-act-iv/album_one_reeler_act_iv.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:izoneapp/data/album.dart';
+import 'package:izoneapp/data/lyrics/one-reeler-act-iv/lyrics_panorama.dart';
 
 class AlbumOneReelerActIV extends Album {
   AlbumOneReelerActIV(BuildContext context)
@@ -8,7 +9,9 @@ class AlbumOneReelerActIV extends Album {
           title: "One-reeler / Act IV",
           releaseDate: "2020-12-07",
           color: Color.fromRGBO(49, 47, 60, 1),
-          songs: [],
+          songs: [
+            LyricsPanorama(context),
+          ],
         );
 
   @override
