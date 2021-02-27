0734ae560e5f36070a1dccef513417bea18c3c70
Eric Nguyen <tuk94307@temple.edu>
2021-02-27 11:26:19 -0500

    Add Beware lyrics

--- lib/data/lyrics/twelve/album_twelve.dart
+++ lib/data/lyrics/twelve/album_twelve.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:izoneapp/data/album.dart';
+import 'package:izoneapp/data/lyrics/twelve/lyrics_beware.dart';
 
 class AlbumTwelve extends Album {
   AlbumTwelve(BuildContext context)
@@ -8,7 +9,9 @@ class AlbumTwelve extends Album {
           title: "Twelve",
           releaseDate: "2020-10-21",
           color: Color.fromRGBO(184, 126, 164, 1),
-          songs: [],
+          songs: [
+            LyricsBeware(context),
+          ],
         );
 
   @override
