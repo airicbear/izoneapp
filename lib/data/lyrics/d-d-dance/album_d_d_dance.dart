00748b20de1a3b9e636baf927cca114fb44eae0d
Eric Nguyen <tuk94307@temple.edu>
2021-02-27 12:09:37 -0500

    Add D-D-Dance lyrics

--- lib/data/lyrics/d-d-dance/album_d_d_dance.dart
+++ lib/data/lyrics/d-d-dance/album_d_d_dance.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:izoneapp/data/album.dart';
+import 'package:izoneapp/data/lyrics/d-d-dance/lyrics_d_d_dance.dart';
 
 class AlbumDDDance extends Album {
   AlbumDDDance(BuildContext context)
@@ -8,7 +9,9 @@ class AlbumDDDance extends Album {
           title: "D-D-Dance",
           releaseDate: "2021-01-26",
           color: Color.fromRGBO(75, 61, 97, 1),
-          songs: [],
+          songs: [
+            LyricsDDDance(context),
+          ],
         );
 
   @override
